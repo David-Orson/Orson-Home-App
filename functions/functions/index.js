a760fee15fca43f649f5192fca5a48d3a83600d9
David Orson <onlyorson@outlook.com>
2020-12-28 21:24:49 +0000

    Firebase init + index

---
+++ functions/functions/index.js
@@ -0,0 +1,15 @@
+const functions = require('firebase-functions');
+const app = require("express")();
+const FBAuth = require("./utility/fbAuth");
+
+const cors = require("cors");
+app.use(cors());
+
+const {
+  signup
+} = require("./handlers/users");
+
+app.post("/signup", signup);
+
+exports.api = functions.region("europe-west1").https.onRequest(app);
+
