89961e0cb13651bbf99d75e6ba1123ac42aa8291
David Orson <onlyorson@outlook.com>
2020-12-28 04:54:01 +0000

    Home and Login Routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Router } from "@reach/router";
 
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
+import Home from './pages/Home';
+import Login from './pages/Login';
+
 const App = () => {
   return (
     <Provider store={store}>
-      <div>
-        hi
-      </div>
+      <Router>
+        <Home path='/' />
+        <Login path='/login' />
+      </Router>
     </Provider>
   )
 }
