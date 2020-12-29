1e57b6f1ea949007c5248c55d3f9a5ed7bfbe3ba
David Orson <onlyorson@outlook.com>
2020-12-29 00:22:25 +0000

    config + validators

---
+++ functions/functions/utility/firebaseConfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiKey: "AIzaSyC8UKreTlhdDoNN3bvSBZZgh8jd-xvR7-o",
+    authDomain: "orson-home-app-ba0cb.firebaseapp.com",
+    projectId: "orson-home-app-ba0cb",
+    storageBucket: "orson-home-app-ba0cb.appspot.com",
+    messagingSenderId: "984984851878",
+    appId: "1:984984851878:web:0de628ec2f577ac010eb67"
+};
---
+++ functions/functions/utility/validators.js
@@ -0,0 +1,47 @@
+const isEmail = (email) => {
+  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(regex)) return true;
+  else return false
+}
+
+const isEmpty = (string) => {
+  if (string.trim() === "") return true;
+  else return false;
+};
+
+exports.validateSignupData = (data) => {
+  let errors = {};
+
+  if (isEmpty(data.email)) {
+    errors.email = "Please enter an Email";
+  } else if (!isEmail(data.email)) {
+    errors.email = "Please enter a valid email address";
+  }
+
+  if (isEmpty(data.password)) errors.password = "Please enter a Password";
+
+  if (data.password !== data.confirmPassword) {
+    errors.confirmPassword = "Passwords must match";
+  }
+
+  if (isEmpty(data.handle)) errors.handle = "Please enter a Username";
+
+  return {
+    errors,
+    valid: Object.keys(errors).length === 0 ? true : false,
+  };
+};
+
+exports.validateLoginData = (data) => {
+  let errors = {};
+
+  if (isEmpty(data.email)) errors.email = "Please enter your email address";
+  if (isEmpty(data.password)) errors.password = "Please enter your Password";
+
+  return {
+    errors,
+    valid: Object.keys(errors).length === 0 ? true : false,
+  };
+};
+
+
